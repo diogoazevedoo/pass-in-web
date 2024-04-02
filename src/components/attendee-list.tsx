7de249f46daa44614e51cf1bb578098ab920ed37
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-04-02 17:08:00 +0100

    Component: create icon-button component to use for table buttons and arrow pagination buttons with varaible to change background color

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,4 +1,5 @@
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from "lucide-react"
+import { IconButton } from "./ui/icon-button"
 
 export function AttendeeList() {
     return (
@@ -44,9 +45,9 @@ export function AttendeeList() {
                                     <td className="py-3 px-4 text-sm text-zinc-300">7 days ago</td>
                                     <td className="py-3 px-4 text-sm text-zinc-300">3 days ago</td>
                                     <td className="py-3 px-4 text-sm text-zinc-300">
-                                        <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
+                                        <IconButton transparent>
                                             <MoreHorizontal className="size-4" />
-                                        </button>
+                                        </IconButton>
                                     </td>
                                 </tr>
                             )
@@ -62,18 +63,18 @@ export function AttendeeList() {
                                     <span>Page 1 of 23</span>
 
                                     <div className="flex gap-1.5">
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        <IconButton>
                                             <ChevronsLeft className="size-4" />
-                                        </button>
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronLeft className="size-4" />
-                                        </button>
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronRight className="size-4" />
-                                        </button>
-                                        <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                                        </IconButton>
+                                        <IconButton>
                                             <ChevronsRight className="size-4" />
-                                        </button>
+                                        </IconButton>
                                     </div>
                                 </div>
                             </td>
