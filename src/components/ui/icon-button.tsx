623b745db4e49717d95c8af3148c4a899e25b285
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-04-03 22:29:33 +0100

    Pagination: implement pagination function

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -10,7 +10,7 @@ export function IconButton({ transparent, ...props }: IconButtonProps) {
             {...props} 
             className={transparent
                 ? "bg-black/20 border border-white/10 rounded-md p-1.5" 
-                : "bg-white/10 border border-white/10 rounded-md p-1.5"
+                : "bg-white/10 border border-white/10 rounded-md p-1.5 disabled:opacity-50"
             }
         />
     )
